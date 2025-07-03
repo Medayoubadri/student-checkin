be06e65a16de74f661842d870665a6dd5aefec23
medayoubadri <medayoubadri@gmail.com>
2025-07-03 23:55:10 +0100

    fixing authentication-6-1

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,13 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/", // The index route
+    "/auth/:path*",
+    "/api/:path*",
+    "/Home/:path*",
+    "/students/:path*",
+    "/(fr|en)/:path*",
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
 };
