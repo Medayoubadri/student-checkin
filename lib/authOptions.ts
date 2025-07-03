571e03f8390c0270bb63c0ba3da2eba06c31b475
medayoubadri <medayoubadri@gmail.com>
2025-07-03 23:50:00 +0100

    fixing authentication-4

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -28,6 +28,17 @@ export const authOptions: NextAuthOptions = {
       }
       return session;
     },
+    redirect: async ({ url, baseUrl }) => {
+      // Check if the URL is relative (starts with a slash)
+      if (url.startsWith("/")) {
+        // Extract locale from the URL or default to 'en'
+        const locale = url.split("/")[1];
+        // Ensure the URL starts with the locale and redirects to the Home page
+        return `${baseUrl}/${locale}/Home`;
+      }
+      // If it's an absolute URL, return it as is
+      return url;
+    },
   },
   secret: process.env.NEXTAUTH_SECRET,
 };
